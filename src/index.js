e2b2e7412f3bd2beac1a8a8316fd693e0effd947
Luis Rodrigues <goblindegook@users.noreply.github.com>
2016-07-30 18:33:36 +0100

    Updated packages, switched to Standard.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 const numerals = {
-  1:    'I',
-  4:    'IV',
-  5:    'V',
-  9:    'IX',
-  10:   'X',
-  40:   'XL',
-  50:   'L',
-  90:   'XC',
-  100:  'C',
-  400:  'CD',
-  500:  'D',
-  900:  'CM',
-  1000: 'M',
+  1: 'I',
+  4: 'IV',
+  5: 'V',
+  9: 'IX',
+  10: 'X',
+  40: 'XL',
+  50: 'L',
+  90: 'XC',
+  100: 'C',
+  400: 'CD',
+  500: 'D',
+  900: 'CM',
+  1000: 'M'
 }
 
 const indices = Object.keys(numerals)
 
 /**
  * Recursively convert Arabic-Indic numerals to their Roman equivalents.
+ *
  * @param  {Number} arabic Numeral between 1-3999.
  * @param  {String} acc    Accumulated output.
  * @return {String}        Converted Roman numeral.
  */
-function recursiveRomanify(arabic, acc = '') {
+function recursiveRomanify (arabic, acc = '') {
   if (!arabic) {
     return acc
   }
@@ -34,10 +35,11 @@ function recursiveRomanify(arabic, acc = '') {
 
 /**
  * Convert Arabic-Indic numerals to their Roman equivalents.
+ *
  * @param  {Number} arabic Numeral between 1-3999.
  * @return {String}        Converted Roman numeral.
  */
-function romanify(arabic) {
+function romanify (arabic) {
   if (arabic < 1 || arabic > 3999) {
     throw new Error('Romani ite domum.')
   }
